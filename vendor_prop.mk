ede197433e9825bad91f4a46046aafc208468350
Pand44X <67182343+Pand44X@users.noreply.github.com>
2021-06-06 21:22:33 -0300

    DotOS

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -1,7 +1,7 @@
-# DPI
+#DPI
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.sf.lcd_density=420
 
-# Fix for echo in calls
+#Fix for echo in calls
 PRODUCT_PROPERTY_OVERRIDES += \
     audio_hal.disable_two_mic=false
